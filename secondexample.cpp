3409d8e6e24c77bc43f8dc05b9c5528d8d20177d
akshita-sheela <82803237+akshita-sheela@users.noreply.github.com>
2023-02-17 11:53:12 +0530

    Update secondexample.cpp

--- secondexample.cpp
+++ secondexample.cpp
@@ -1,15 +1,3 @@
-/** @file doxygen_example.cpp
-@author Lastname:Firstname:A00123456:cscxxxxx
-@version Revision 1.1
-@brief Illustrates doxygen-style comments for documenting a C++
-program file and the functions in that file.
-@details If you want to add any further detailed description of
-what is in the file, then place it here (after the first statement)
-and it will appear in the detailed description section of the HTML
-output description for the file.
-@date Monday, September 19, 2011
-*/
-
 /**
 @mainpage
 This short program contains some sample code illustrating
